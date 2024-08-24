b1e4cb152e0a7cf25e4ded69bfb5b492e3e9332f
amaan-bhati <amaanbhati49@gmail.com>
2024-08-24 20:18:43 +0530

    update-test

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ import FileUpload from './components/FileUpload';
 export default function Home() {
   return (
     <>
+   
+      <h1 className='m-2 text-2xl font-semibold'>upload csv</h1>
+    
       <div className="flex items-center justify-center h-screen w-full bg-white">
+        
         <div className="w-full">
           <div className="m-auto  w-[430px] h-[330px] p-4 ">
             
